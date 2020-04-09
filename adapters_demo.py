8e3837fabd0b871c761e732ffabcc73026e0ebad
calpt <36051308+calpt@users.noreply.github.com>
2020-04-09 21:04:00 +0200

    Use config hash as dir name. default_config.

--- adapters_demo.py
+++ adapters_demo.py
@@ -2,7 +2,6 @@ import torch
 
 from transformers.modeling_bert import BertModel
 from combine_adapters import copy_adapter_weights
-from transformers.adapter import *
 
 
 def is_output_equal(model1, model2, adapters=None, iterations=1, input_shape=(1, 128)):
@@ -26,6 +25,7 @@ def is_output_equal(model1, model2, adapters=None, iterations=1, input_shape=(1,
         results.append(torch.equal(output1, output2))
     return all(results)
 
+
 def is_model_equal(model1, model2):
     """Checks whether all parameters of two models are equal."""
     results = []
@@ -33,27 +33,30 @@ def is_model_equal(model1, model2):
         results.append(torch.equal(param1.data, param2.data))
     return all(results)
 
+
 def model_diff(model1, model2):
-    params1 = [n for (n,v) in model1.named_parameters()]
-    params2 = [n for (n,v) in model2.named_parameters()]
-    return [s for s in params1 if not s in params2], [s for s in params2 if not s in params1]
+    params1 = [n for (n, v) in model1.named_parameters()]
+    params2 = [n for (n, v) in model2.named_parameters()]
+    return [s for s in params1 if s not in params2], [s for s in params2 if s not in params1]
+
 
 def _print_params(model):
     for s, _ in model.named_parameters():
         print(s)
+
 
 def run_adapter_test():
     ### A little demo checking the correctness of adapter saving/ loading ###
     global bert_sst
 
     # load BERT with SST adapter included
-    bert_sst = BertModel.from_pretrained(MODEL_DIR+"sst")
+    bert_sst = BertModel.from_pretrained(MODEL_DIR + "sst")
     # load two default BERTs from huggingface
     bert_add_old = BertModel.from_pretrained("bert-base-uncased")
     bert_add_new = BertModel.from_pretrained("bert-base-uncased")
 
     # save the SST adapter to the file system
-    bert_sst.save_adapter(ADAPTER_DIR+"sst", "sst")
+    bert_sst.save_adapter(ADAPTER_DIR + "sst", "sst")
 
     # add SST adapter to BERT using old method
     bert_add_old.config.adapters = []
@@ -61,7 +64,7 @@ def run_adapter_test():
     copy_adapter_weights(bert_sst, bert_add_old)
 
     # add SST adapter to BERT by loading the previously saved
-    bert_add_new.load_adapter(ADAPTER_DIR+"sst")
+    bert_add_new.load_adapter(ADAPTER_DIR + "sst")
 
     # check equality
     assert is_output_equal(bert_add_new, bert_sst, adapters=['sst'])
@@ -70,6 +73,23 @@ def run_adapter_test():
     assert is_model_equal(bert_add_new, bert_add_old)
 
     _print_params(bert_add_new)
+
+
+def run_adapter_download_test():
+    global bert_sst
+
+    # load BERT with SST adapter included
+    bert_sst = BertModel.from_pretrained(MODEL_DIR + "sst")
+
+    # download pretrained adapter
+    bert_base = BertModel.from_pretrained("bert-base-uncased")
+    bert_base.load_adapter("sst")
+
+    # check equality
+    for k, v in bert_sst.config.adapter_config.items():
+        assert bert_base.config.adapter_config[k] == v
+    assert is_output_equal(bert_sst, bert_base, adapters=['sst'])
+    assert is_model_equal(bert_sst, bert_base)
 
 
 if __name__ == "__main__":
@@ -81,4 +101,5 @@ if __name__ == "__main__":
     MODEL_DIR = "../data/Adapters_16_Bert_Base/"
     ADAPTER_DIR = "../data/adapters/"
 
-    run_adapter_test()
+    # run_adapter_test()
+    run_adapter_download_test()
--- src/transformers/adapters_model.py
+++ src/transformers/adapters_model.py
@@ -3,23 +3,43 @@ from os.path import isdir, isfile, join
 import torch
 import json
 from transformers.file_utils import is_remote_url, get_from_cache, torch_cache_home
-from .adapters_utils import find_matching_config_path, urljoin
+from .adapters_utils import find_matching_config_path, urljoin, CONFIG_NAME, WEIGHTS_NAME
 
 
 logger = logging.getLogger(__name__)
-
-CONFIG_NAME = "adapter_config.json"
-WEIGHTS_NAME = "pytorch_adapter.bin"
 
 # the download cache
 ADAPTER_CACHE = join(torch_cache_home, "adapters")
 
 PRETRAINED_TASK_ADAPTER_MAP = {
-
+    "csqa": "http://adapter-hub.webredirect.org/task-csqa/",
+    "hellaswag": "http://adapter-hub.webredirect.org/task-hellaswag/",
+    "sst": "http://adapter-hub.webredirect.org/task-sst/",
+    "dummy": "http://adapter-hub.webredirect.org/task-dummy/"
 }
+
 PRETRAINED_LANG_ADAPTER_MAP = {
-
+    "xyz": "http://adapter-hub.webredirect.org/lang-xyz/"
 }
+
+ADAPTER_CONFIG_MAP = {
+    'pfeiffer': {
+        'LN_after': False,
+        'LN_before': False,
+        'MH_Adapter': False,
+        'Output_Adapter': True,
+        'adapter_residual_before_ln': False,
+        'attention_type': 'sent-lvl-dynamic',
+        'new_attention_norm': False,
+        'non_linearity': 'relu',
+        'original_ln_after': True,
+        'original_ln_before': True,
+        'reduction_factor': 16,
+        'residual_before_ln': True
+    }
+}
+
+DEFAULT_ADAPTER_CONFIG = 'pfeiffer'
 
 
 class AdaptersModelMixin:
@@ -27,15 +47,14 @@ class AdaptersModelMixin:
 
     def adapter_state_dict(self, task_name):
         """Returns a dictionary containing the whole state of the specified task adapter.
-        
+
         Args:
             task_name (str): the name of the task adapter
         """
         # TODO we have a problem if the model contains adapters of different types w. the same name
         return {k:v for (k,v) in self.state_dict().items() if 'adapters.{}'.format(task_name) in k}
 
-    # TODO currently expects a pre-defined adapter config
-    def adapter_save_config(self, adapter_type, name=None):
+    def adapter_save_config(self, adapter_type, name=None, default_config=None):
         config_dict = {
             'type': adapter_type,
             'model': self.__class__.__name__, 'hidden_size': self.config.hidden_size
@@ -43,9 +62,9 @@ class AdaptersModelMixin:
         if name:
             config_dict['name'] = name
         if adapter_type == 'task':
-            config_dict['config'] = getattr(self.config, 'adapter_config', None)
+            config_dict['config'] = getattr(self.config, "adapter_config", default_config)
         elif adapter_type == 'lang':
-            config_dict['config'] = getattr(self.config, "language_adapter_config", None)
+            config_dict['config'] = getattr(self.config, "language_adapter_config", default_config)
         else:
             raise ValueError("Invalid adapter type {}".format(adapter_type))
         return config_dict
@@ -53,7 +72,7 @@ class AdaptersModelMixin:
     def save_adapter(self, save_directory, task_name):
         """Saves a task adapter and its configuration file to a directory, so that it can be reloaded
         using the `model.load_adapter()` method.
-        
+
         Args:
             save_directory (str): a path to a directory where the adapter will be saved
             task_name (str): the name of the task adapter to be saved
@@ -66,7 +85,7 @@ class AdaptersModelMixin:
     def save_language_adapter(self, save_directory, language_name):
         """Saves a language adapter and its configuration file to a directory, so that it can be reloaded
         using the `model.load_adapter()` method.
-        
+
         Args:
             save_directory (str): a path to a directory where the adapter will be saved
             task_name (str): the name of the language adapter to be saved
@@ -92,22 +111,22 @@ class AdaptersModelMixin:
         torch.save(adapter_state_dict, output_file)
         logger.info("Adapter weights saved in {}".format(output_file))
 
-    def load_adapter(self, adapter_name_or_path, **kwargs):
+    def load_adapter(self, adapter_name_or_path, default_config=DEFAULT_ADAPTER_CONFIG, **kwargs):
         """Loads a pre-trained pytorch task adapter from an adapter configuration.
-        
+
         Args:
             adapter_name_or_path (str): path to a directory containing adapter weights saved using `model.saved_adapter()`
-        
+
         Returns:
             (dict, dict): tuple containing missing_adapter_keys and unexpected_keys
         """
         cache_dir = kwargs.pop("cache_dir", ADAPTER_CACHE)
 
         # Resolve the weights to be loaded based on the given identifier and the current adapter config
-        weights_file, config_file, needs_download = self._resolve_adapter_path(adapter_name_or_path, 'task')
+        weights_file, config_file = self._resolve_adapter_path(adapter_name_or_path, 'task', default_config)
 
         # Load config of adapter
-        config = self._load_adapter_config(config_file, needs_download, cache_dir=cache_dir, **kwargs)
+        config = self._load_adapter_config(config_file, cache_dir=cache_dir, **kwargs)
         assert config['type'] == 'task', "Loaded adapter has to be a task adapter."
         # If no adapter config is available yet, set to the config of the loaded adapter
         if not hasattr(self.config, "adapter_config"):
@@ -118,24 +137,24 @@ class AdaptersModelMixin:
             for k,v in config['config'].items():
                 assert self.config.adapter_config[k] == v, "Adapter configurations have to be equal."
 
-        return self._load_adapter_weights(weights_file, needs_download, config, cache_dir=cache_dir, **kwargs)
+        return self._load_adapter_weights(weights_file, config, cache_dir=cache_dir, **kwargs)
 
-    def load_language_adapter(self, adapter_name_or_path, **kwargs):
+    def load_language_adapter(self, adapter_name_or_path, default_config=DEFAULT_ADAPTER_CONFIG, **kwargs):
         """Loads a pre-trained pytorch language adapter from an adapter configuration.
-        
+
         Args:
             adapter_name_or_path (str): path to a directory containing adapter weights saved using `model.saved_adapter()`
-        
+
         Returns:
             (dict, dict): tuple containing missing_adapter_keys and unexpected_keys
         """
         cache_dir = kwargs.pop("cache_dir", ADAPTER_CACHE)
 
         # Resolve the weights to be loaded based on the given identifier and the current adapter config
-        weights_file, config_file, needs_download = self._resolve_adapter_path(adapter_name_or_path, 'task')
+        weights_file, config_file = self._resolve_adapter_path(adapter_name_or_path, 'lang', default_config)
 
         # Load config of adapter
-        config = self._load_adapter_config(config_file, needs_download, cache_dir=cache_dir, **kwargs)
+        config = self._load_adapter_config(config_file, cache_dir=cache_dir, **kwargs)
         assert config['type'] == 'lang', "Loaded adapter has to be a language adapter."
         # If no adapter config is available yet, set to the config of the loaded adapter
         if not hasattr(self.config, "language_adapter_config"):
@@ -146,13 +165,13 @@ class AdaptersModelMixin:
             for k,v in config['config'].items():
                 assert self.config.language_adapter_config[k] == v, "Adapter configurations have to be equal."
 
-        return self._load_adapter_weights(weights_file, needs_download, config, cache_dir=cache_dir, **kwargs)
+        return self._load_adapter_weights(weights_file, config, cache_dir=cache_dir, **kwargs)
 
-    def _load_adapter_config(self, resolved_file, needs_download, **kwargs):
+    def _load_adapter_config(self, resolved_file, **kwargs):
         """Loads an adapter configuration.
         """
         # If necessary, download the config or load it from cache
-        if needs_download:
+        if is_remote_url(resolved_file):
             config_file = get_from_cache(resolved_file, **kwargs)
             if not config_file:
                 raise EnvironmentError(
@@ -169,11 +188,11 @@ class AdaptersModelMixin:
 
         return adapter_config
 
-    def _load_adapter_weights(self, resolved_file, needs_download, config, **kwargs):
+    def _load_adapter_weights(self, resolved_file, config, **kwargs):
         """Loads adapter weights.
         """
         # If necessary, download the weights or load them from cache
-        if needs_download:
+        if is_remote_url(resolved_file):
             weights_file = get_from_cache(resolved_file, **kwargs)
             if not weights_file:
                 raise EnvironmentError(
@@ -189,11 +208,9 @@ class AdaptersModelMixin:
         if config['type'] == 'task':
             if not name in self.config.adapters:
                 self.add_adapter(name)
-                self.config.adapters.append(name)
         elif config['type'] == 'lang':
             if not name in self.config.language_adapters:
                 self.add_language_adapter(name)
-                self.config.language_adapters.append(name)
 
         # Load the weights of the adapter
         try:
@@ -208,8 +225,8 @@ class AdaptersModelMixin:
         missing_adapter_keys = [k for k in missing_keys if 'adapters.{}'.format(config['name']) in k]
         if len(missing_adapter_keys) > 0:
             logger.warn(
-               "Some adapter weights could not be found in loaded weights file: {}".format(
-                   ', '.join(missing_adapter_keys)) 
+                "Some adapter weights could not be found in loaded weights file: {}".format(
+                    ', '.join(missing_adapter_keys))
             )
         if len(unexpected_keys) > 0:
             logger.warn(
@@ -219,32 +236,33 @@ class AdaptersModelMixin:
 
         return missing_adapter_keys, unexpected_keys
 
-    def _resolve_adapter_path(self, adapter_name_or_path, adapter_type):
+    def _resolve_adapter_path(self, adapter_name_or_path, adapter_type, default_config):
+        assert default_config in ADAPTER_CONFIG_MAP, "Specified default config is invalid."
         # task adapter with identifier
         if adapter_type == 'task' and adapter_name_or_path in PRETRAINED_TASK_ADAPTER_MAP:
-            config = self.adapter_save_config('task')
+            config = self.adapter_save_config('task', default_config=ADAPTER_CONFIG_MAP[default_config])
             resolved_path = find_matching_config_path(PRETRAINED_TASK_ADAPTER_MAP[adapter_name_or_path], config)
-            return urljoin(resolved_path, WEIGHTS_NAME), urljoin(resolved_path, CONFIG_NAME), True
+            return urljoin(resolved_path, WEIGHTS_NAME), urljoin(resolved_path, CONFIG_NAME)
         # language adapter with identifier
         elif adapter_type == 'lang' and adapter_name_or_path in PRETRAINED_LANG_ADAPTER_MAP:
-            config = self.adapter_save_config('lang')
+            config = self.adapter_save_config('lang', default_config=ADAPTER_CONFIG_MAP[default_config])
             resolved_path = find_matching_config_path(PRETRAINED_LANG_ADAPTER_MAP[adapter_name_or_path], config)
-            return urljoin(resolved_path, WEIGHTS_NAME), urljoin(resolved_path, CONFIG_NAME), True
+            return urljoin(resolved_path, WEIGHTS_NAME), urljoin(resolved_path, CONFIG_NAME)
         # url of a folder containing pretrained adapters
         elif is_remote_url(adapter_name_or_path):
-            config = self.adapter_save_config(adapter_type) 
+            config = self.adapter_save_config(adapter_type, default_config=ADAPTER_CONFIG_MAP[default_config])
             resolved_path = find_matching_config_path(adapter_name_or_path, config)
-            return urljoin(resolved_path, WEIGHTS_NAME), urljoin(resolved_path, CONFIG_NAME), True
+            return urljoin(resolved_path, WEIGHTS_NAME), urljoin(resolved_path, CONFIG_NAME)
         # path to a local folder saved using save_adapter() or save_language_adapter()
         elif isdir(adapter_name_or_path):
             if isfile(join(adapter_name_or_path, WEIGHTS_NAME)) and isfile(join(adapter_name_or_path, CONFIG_NAME)):
                 weights_file = join(adapter_name_or_path, WEIGHTS_NAME)
                 config_file = join(adapter_name_or_path, CONFIG_NAME)
-                return weights_file, config_file, False
+                return weights_file, config_file
             else:
                 raise EnvironmentError(
                     "No file {} or no file {} found in directory {}".format(
                         WEIGHTS_NAME, CONFIG_NAME, adapter_name_or_path)
-                    )
+                )
         else:
             raise ValueError("Unable to identify {} as a valid module location.".format(adapter_name_or_path))
