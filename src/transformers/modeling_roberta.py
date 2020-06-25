afb2974844937d5b430ad7131ff9994cdc66d9ca
calpt <36051308+calpt@users.noreply.github.com>
2020-06-25 10:34:12 +0200

    Reduced test set. Test fixes.

--- src/transformers/modeling_roberta.py
+++ src/transformers/modeling_roberta.py
@@ -190,7 +190,7 @@ class RobertaModelWithHeads(BertModelHeadsMixin, BertPreTrainedModel):
         language=None,
         head=None,
     ):
-        input_ids = input_ids.view(-1, input_ids.size(-1))
+        input_ids = input_ids.view(-1, input_ids.size(-1)) if input_ids is not None else None
         attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
         token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1)) if token_type_ids is not None else None
         position_ids = position_ids.view(-1, position_ids.size(-1)) if position_ids is not None else None
