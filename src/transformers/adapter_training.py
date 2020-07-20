481af79abfaadaebb037900a38473af1ed4bfff2
calpt <36051308+calpt@users.noreply.github.com>
2020-07-20 21:41:29 +0200

    Fix loading prediction heads of task adapters

--- src/transformers/adapter_training.py
+++ src/transformers/adapter_training.py
@@ -37,21 +37,22 @@ def setup_task_adapter_training(model, task_name: str, adapter_args: AdapterArgu
     """
     language = adapter_args.load_lang_adapter
     if adapter_args.train_adapter:
-        # get actual model for derived models with heads
-        base_model = getattr(model, model.base_model_prefix, model)
         # task adapter - only add if not existing
-        if task_name not in base_model.config.adapters.adapter_list(AdapterType.text_task):
-            base_model.set_adapter_config(AdapterType.text_task, adapter_args.adapter_config)
+        if task_name not in model.config.adapters.adapter_list(AdapterType.text_task):
             # load a pre-trained adapter for fine-tuning if specified
             if adapter_args.load_task_adapter:
-                base_model.load_adapter(adapter_args.load_task_adapter, AdapterType.text_task, load_as=task_name)
+                model.load_adapter(
+                    adapter_args.load_task_adapter,
+                    AdapterType.text_task,
+                    config=adapter_args.adapter_config,
+                    load_as=task_name,
+                )
             # otherwise, add a new adapter
             else:
-                base_model.add_adapter(task_name, AdapterType.text_task)
+                model.add_adapter(task_name, AdapterType.text_task, config=adapter_args.adapter_config)
         # language adapter - only add if not existing
-        if language and language not in base_model.config.adapters.adapter_list(AdapterType.text_lang):
+        if language and language not in model.config.adapters.adapter_list(AdapterType.text_lang):
             lconfig_string = adapter_args.lang_adapter_config or adapter_args.adapter_config
-            base_model.set_adapter_config(AdapterType.text_lang, lconfig_string)
             # TODO support different non_linearity & reduction_factor
             lconfig = AdapterConfig.load(lconfig_string, non_linearity="gelu", reduction_factor=2)
 
@@ -59,7 +60,7 @@ def setup_task_adapter_training(model, task_name: str, adapter_args: AdapterArgu
                 adapter_args.load_lang_adapter, AdapterType.text_lang, config=lconfig, load_as=adapter_args.language
             )
         # enable adapter training
-        base_model.train_adapter([task_name])
+        model.train_adapter([task_name])
     # set adapters as default if possible
     if isinstance(model, BertModelHeadsMixin):
         adapter_names = []
