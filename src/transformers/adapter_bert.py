0bad935235b0537259fa2b0e2de410f6f4f2106c
calpt <calpt@mail.de>
2021-03-22 11:13:42 +0100

    Flex head implementation refactorings (#129)
    * Simplify flex head mixin implementation
    
    * Keep in hidden_size in prediction head if no num_labels or num_choices given
    
    * Style fix

--- src/transformers/adapter_bert.py
+++ src/transformers/adapter_bert.py
@@ -131,65 +131,17 @@ class BertModelAdaptersMixin(InvertibleAdaptersMixin, ModelAdaptersMixin):
 
 
 class BertModelHeadsMixin(ModelWithFlexibleHeadsAdaptersMixin):
-    """Adds flexible heads to a BERT-based model class."""
+    """
+    Adds flexible heads to a BERT-based model class.
+    """
 
-    def add_prediction_head_from_config(self, head_name, config, overwrite_ok=False):
-        id2label = (
-            {id_: label for label, id_ in config["label2id"].items()}
-            if "label2id" in config.keys() and config["label2id"]
-            else None
-        )
-        if config["head_type"] == "classification":
-            self.add_classification_head(
-                head_name,
-                config["num_labels"],
-                config["layers"],
-                config["activation_function"],
-                id2label=id2label,
-                overwrite_ok=overwrite_ok,
-            )
-        elif config["head_type"] == "multilabel_classification":
-            self.add_classification_head(
-                head_name,
-                config["num_labels"],
-                config["layers"],
-                config["activation_function"],
-                multilabel=True,
-                id2label=id2label,
-                overwrite_ok=overwrite_ok,
-            )
-        elif config["head_type"] == "tagging":
-            self.add_tagging_head(
-                head_name,
-                config["num_labels"],
-                config["layers"],
-                config["activation_function"],
-                id2label=id2label,
-                overwrite_ok=overwrite_ok,
-            )
-        elif config["head_type"] == "multiple_choice":
-            self.add_multiple_choice_head(
-                head_name,
-                config["num_choices"],
-                config["layers"],
-                config["activation_function"],
-                id2label=id2label,
-                overwrite_ok=overwrite_ok,
-            )
-        elif config["head_type"] == "question_answering":
-            self.add_qa_head(
-                head_name,
-                config["num_labels"],
-                config["layers"],
-                config["activation_function"],
-                id2label=id2label,
-                overwrite_ok=overwrite_ok,
-            )
-        else:
-            if config["head_type"] in self.config.custom_heads:
-                self.add_custom_head(head_name, config, overwrite_ok=overwrite_ok)
-            else:
-                raise AttributeError("Please register the PredictionHead before loading the model")
+    head_types = {
+        "classification": ClassificationHead,
+        "multilabel_classification": MultiLabelClassificationHead,
+        "tagging": TaggingHead,
+        "multiple_choice": MultipleChoiceHead,
+        "question_answering": QuestionAnsweringHead,
+    }
 
     def add_classification_head(
         self,
@@ -214,9 +166,9 @@ class BertModelHeadsMixin(ModelWithFlexibleHeadsAdaptersMixin):
         """
 
         if multilabel:
-            head = MultiLabelClassificationHead(head_name, num_labels, layers, activation_function, id2label, self)
+            head = MultiLabelClassificationHead(self, head_name, num_labels, layers, activation_function, id2label)
         else:
-            head = ClassificationHead(head_name, num_labels, layers, activation_function, id2label, self)
+            head = ClassificationHead(self, head_name, num_labels, layers, activation_function, id2label)
         self.add_prediction_head(head, overwrite_ok)
 
     def add_multiple_choice_head(
@@ -232,7 +184,7 @@ class BertModelHeadsMixin(ModelWithFlexibleHeadsAdaptersMixin):
             activation_function (str, optional): Activation function. Defaults to 'tanh'.
             overwrite_ok (bool, optional): Force overwrite if a head with the same name exists. Defaults to False.
         """
-        head = MultipleChoiceHead(head_name, num_choices, layers, activation_function, id2label, self)
+        head = MultipleChoiceHead(self, head_name, num_choices, layers, activation_function, id2label)
         self.add_prediction_head(head, overwrite_ok)
 
     def add_tagging_head(
@@ -248,11 +200,11 @@ class BertModelHeadsMixin(ModelWithFlexibleHeadsAdaptersMixin):
             activation_function (str, optional): Activation function. Defaults to 'tanh'.
             overwrite_ok (bool, optional): Force overwrite if a head with the same name exists. Defaults to False.
         """
-        head = TaggingHead(head_name, num_labels, layers, activation_function, id2label, self)
+        head = TaggingHead(self, head_name, num_labels, layers, activation_function, id2label)
         self.add_prediction_head(head, overwrite_ok)
 
     def add_qa_head(
         self, head_name, num_labels=2, layers=1, activation_function="tanh", overwrite_ok=False, id2label=None
     ):
-        head = QuestionAnsweringHead(head_name, num_labels, layers, activation_function, id2label, self)
+        head = QuestionAnsweringHead(self, head_name, num_labels, layers, activation_function, id2label)
         self.add_prediction_head(head, overwrite_ok)
