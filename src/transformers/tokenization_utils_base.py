43e08b1244e968f5e1af19e6671b8f245a8c723b
Hannah Sterz <hannah.sterztud.tu-darmstadt.de>
2021-03-28 08:27:57 +0200

    Fix

--- src/transformers/tokenization_utils_base.py
+++ src/transformers/tokenization_utils_base.py
@@ -196,7 +196,7 @@ class BatchEncoding(UserDict):
         self._encodings = encoding
 
         if n_sequences is None and encoding is not None and len(encoding):
-            n_sequences = len(encoding)
+            n_sequences = encoding[0].n_sequences
 
         self._n_sequences = n_sequences
 
