7f31ddebcf451359acb068cdc15701a8ef76a65e
hSterz <hsterz16@gmail.com>
2021-09-06 12:58:51 +0200

    Added tset

--- tests/test_adapter_trainer.py
+++ tests/test_adapter_trainer.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from tempfile import TemporaryDirectory
 
@@ -13,7 +14,7 @@ from transformers import (
     GlueDataTrainingArguments,
     TrainingArguments,
 )
-from transformers.adapters.composition import Fuse
+from transformers.adapters.composition import Fuse, Stack
 from transformers.adapters.trainer import AdapterTrainer as Trainer
 from transformers.testing_utils import slow
 
@@ -259,8 +260,59 @@ class TestAdapterTrainer(unittest.TestCase):
                 if "adapter" in k1 or "dummy" in k1:
                     self.assertTrue(torch.equal(v1, v2), k1)
 
-    def test_XY(self):
-        pass
+    def test_general(self):
+        tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+        data_args = GlueDataTrainingArguments(
+            task_name="mrpc", data_dir="./tests/fixtures/tests_samples/MRPC", overwrite_cache=True
+        )
+        train_dataset = GlueDataset(data_args, tokenizer=tokenizer, mode="train")
+
+        model = AutoModelWithHeads.from_pretrained("bert-base-uncased")
+
+        model.add_classification_head("task", num_labels=3)
+
+        # add the adapters to be fused
+        model.add_adapter("task")
+        model.add_adapter("additional_adapter")
+
+        model.train_adapter("task")
+        self.assertEqual("task", model.active_head)
+        self.assertEqual(Stack("task"), model.active_adapters)
+        with TemporaryDirectory() as tempdir:
+            training_args = TrainingArguments(
+                output_dir=tempdir,
+                do_train=True,
+                learning_rate=0.1,
+                logging_steps=1,
+                max_steps=1,
+                save_steps=1,
+                remove_unused_columns=False,
+            )
+            trainer = Trainer(
+                model=model,
+                args=training_args,
+                train_dataset=train_dataset,
+                do_save_adapters=True,
+                do_save_full_model=False,
+            )
+
+            trainer.train()
+
+            self.assertFalse(trainer.do_save_full_model)
+            self.assertTrue(trainer.do_save_adapters)
+            self.assertFalse(trainer.do_save_adapter_fusion)
+
+            # Check that adapters are actually saved but the full model is not
+            files_dir_checkpoint = [file_or_dir for file_or_dir in os.listdir(os.path.join(tempdir, "checkpoint-1"))]
+            self.assertTrue("task" in files_dir_checkpoint)
+            self.assertTrue("additional_adapter" in files_dir_checkpoint)
+            # Check that full model weights are not stored
+            self.assertFalse("pytorch_model.bin" in files_dir_checkpoint)
+
+            # this should always be false in the adapter trainer
+            self.assertFalse(trainer.args.remove_unused_columns)
+            self.assertEqual("task", model.active_head)
+            self.assertEqual(Stack("task"), model.active_adapters)
 
 
 if __name__ == "__main__":
