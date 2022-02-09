Fix prefix tuning attention mask

src/transformers/adapters/prefix_tuning.py
@@ -144,7 +144,7 @@ class PrefixTuningLayer(AdapterLayerBase):
         else:
             return None
 
-    def forward(self, key_states, value_states, attention_mask=None):
+    def forward(self, key_states, value_states, attention_mask=None, invert_mask=True):
         if hasattr(self.config, "adapters"):
             # First check current context before falling back to defined setup
             context = AdapterSetup.get_context()
@@ -175,6 +175,8 @@ class PrefixTuningLayer(AdapterLayerBase):
                             prefix_mask = torch.ones(batch_size, 1, attention_mask.size(2), prefix_keys.size(2)).to(
                                 attention_mask.device
                             )
+                        if invert_mask:
+                            prefix_mask = 1.0 - prefix_mask
                         attention_mask = torch.cat([prefix_mask, attention_mask], dim=-1)
             else:
                 raise ValueError(f"Invalid adapter setup. Cannot use {adapter_setup} with prefix tuning.")

src/transformers/models/distilbert/modeling_distilbert.py
@@ -210,7 +210,7 @@ class MultiHeadSelfAttention(nn.Module):
         k = shape(self.k_lin(key))  # (bs, n_heads, k_length, dim_per_head)
         v = shape(self.v_lin(value))  # (bs, n_heads, k_length, dim_per_head)
 
-        k, v, mask = self.prefix_tuning(k, v, mask)
+        k, v, mask = self.prefix_tuning(k, v, mask, invert_mask=False)
 
         mask_reshp = (bs, 1, 1, k.size(2))
 