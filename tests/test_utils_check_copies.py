96870f969228d5f2c8e2e717ce0d2cac8767959c
calpt <36051308+calpt@users.noreply.github.com>
2022-03-14 18:37:21 +0100

    Unswizzle another test class

--- tests/test_utils_check_copies.py
+++ tests/test_utils_check_copies.py
@@ -43,10 +43,8 @@ REFERENCE_CODE = """    def __init__(self, config):
         # Need a link between the two variables so that the bias is correctly resized with `resize_token_embeddings`
         self.decoder.bias = self.bias
 
-    def forward(self, hidden_states, inv_lang_adapter=None):
+    def forward(self, hidden_states):
         hidden_states = self.transform(hidden_states)
-        if inv_lang_adapter:
-            hidden_states = inv_lang_adapter(hidden_states, rev=True)
         hidden_states = self.decoder(hidden_states)
         return hidden_states
 """
