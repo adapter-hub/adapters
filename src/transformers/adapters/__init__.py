2834411f0f86672401e9e1a0106528fa4bdb0ff9
calpt <36051308+calpt@users.noreply.github.com>
2022-03-23 19:57:35 +0100

    Release v3.0.0

--- src/transformers/adapters/__init__.py
+++ src/transformers/adapters/__init__.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "3.0.0a0"
+__version__ = "3.0.0"
 
 from typing import TYPE_CHECKING
 
