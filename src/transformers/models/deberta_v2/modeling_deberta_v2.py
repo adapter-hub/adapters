58d9d3aefc871e43b2696f6f6e309b053e1d85d4
Hannah <hsterz16@gmail.com>
2022-05-24 14:25:58 +0200

    Style

--- src/transformers/models/deberta_v2/modeling_deberta_v2.py
+++ src/transformers/models/deberta_v2/modeling_deberta_v2.py
@@ -29,7 +29,6 @@ from ...adapters.context import ForwardContext
 from ...adapters.mixins.bert import BertModelAdaptersMixin, BertOutputAdaptersMixin, BertSelfOutputAdaptersMixin
 from ...adapters.model_mixin import ModelWithHeadsAdaptersMixin
 from ...adapters.prefix_tuning import PrefixTuningShim
-from ...file_utils import add_code_sample_docstrings, add_start_docstrings, add_start_docstrings_to_model_forward
 from ...modeling_outputs import (
     BaseModelOutput,
     MaskedLMOutput,
