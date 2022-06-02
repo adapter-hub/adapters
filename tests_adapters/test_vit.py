d0058c3b29dc8cf734a0d3a0dc8df67f171521ba
calpt <36051308+calpt@users.noreply.github.com>
2022-06-02 11:26:28 +0200

    WIP

---
+++ tests_adapters/test_vit.py
@@ -0,0 +1,60 @@
+import unittest
+
+from tests.models.vit.test_modeling_vit import *
+from transformers import ViTAdapterModel
+from transformers.testing_utils import require_torch
+
+from .methods import BottleneckAdapterTestMixin, CompacterTestMixin, LoRATestMixin, PrefixTuningTestMixin
+from .test_adapter import AdapterTestBase, make_config
+from .test_adapter_backward_compability import CompabilityTestMixin
+from .test_adapter_composition import ParallelAdapterInferenceTestMixin, ParallelTrainingMixin
+from .test_adapter_conversion import ModelClassConversionTestMixin
+from .test_adapter_embeddings import EmbeddingTestMixin
+from .test_adapter_fusion_common import AdapterFusionModelTestMixin
+from .test_adapter_heads import PredictionHeadModelTestMixin
+from .test_common import AdapterModelTesterMixin
+
+
+@require_torch
+class ViTAdapterModelTest(AdapterModelTesterMixin, ViTModelTest):
+    all_model_classes = (
+        ViTAdapterModel,
+    )
+
+
+class ViTAdapterTestBase(AdapterTestBase):
+    config_class = ViTConfig
+    config = make_config(
+        ViTConfig,
+        hidden_size=32,
+        num_hidden_layers=4,
+        num_attention_heads=4,
+        intermediate_size=37,
+    )
+
+
+@require_torch
+class ViTAdapterTest(
+    BottleneckAdapterTestMixin,
+    CompacterTestMixin,
+    LoRATestMixin,
+    PrefixTuningTestMixin,
+    EmbeddingTestMixin,
+    AdapterFusionModelTestMixin,
+    CompabilityTestMixin,
+    PredictionHeadModelTestMixin,
+    ParallelAdapterInferenceTestMixin,
+    ParallelTrainingMixin,
+    ViTAdapterTestBase,
+    unittest.TestCase,
+):
+    pass
+
+
+@require_torch
+class ViTClassConversionTest(
+    ModelClassConversionTestMixin,
+    ViTAdapterTestBase,
+    unittest.TestCase,
+):
+    pass
