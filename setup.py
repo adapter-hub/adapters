5635d506f5c45b8b565a93116165bdb1acf7cd20
calpt <calpt@mail.de>
2022-08-24 12:19:47 +0200

    Temporarily pin hf-doc-builder==0.3.0

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ _deps = [
     "ftfy",
     "fugashi>=1.0",
     "GitPython<3.1.19",
-    "hf-doc-builder>=0.3.0",
+    "hf-doc-builder==0.3.0",
     "huggingface-hub>=0.1.0,<0.8.0",
     "importlib_metadata",
     "ipadic>=1.0.0,<2.0",
