366267da3f0b37cc73063d84c2b774b124c582f5
Soham Chakraborty <schakraborty@ischoolconnect.com>
2022-09-06 00:06:53 +0530

    minor

--- my_test.py
+++ my_test.py
@@ -1,41 +1,3 @@
-# # Test for Roberta
-# import torch
-# import os 
-# import sys 
-# fpath = os.path.join(os.path.dirname(__file__),'src')
-# sys.path.append(fpath)
-# print(sys.path)
-
-
-# # from pathlib import Path
-# # from transformers import RobertaTokenizer 
-
-# import src.transformers
-
-# # from transformers.configuration_utils import PretrainedConfig
-# # from transformers.tokenization_utils import PreTrainedTokenizer
-# # from transformers.utils import TensorType
-# # from transformers.utils import is_torch_available
-
-
-# # from src.transformers.models.roberta import RobertaConfig, RobertaTokenizer
-# # from src.transformers.adapters import RobertaModelWithHeads
-
-# from src.transformers import (RobertaTokenizer, 
-#                           RobertaConfig, 
-#                           RobertaModelWithHeads,
-#                           TrainingArguments, 
-#                           AdapterTrainer, 
-#                           EvalPrediction, 
-#                           TextClassificationPipeline)
-# import src.transformers.adapters.composition as ac
-# from src.transformers.adapters.composition import Fuse
-# model_ckpt = "roberta-base"
-# tokenizer = RobertaTokenizer.from_pretrained(model_ckpt)
-# print("hello world")
-
-#========================================================================================
-
 # Test for Roberta
 import torch
 from datasets import load_dataset
@@ -60,8 +22,10 @@ from src.transformers import (BigBirdTokenizer,
                           TextClassificationPipeline)
 
 
+
 import src.transformers.adapters.composition as ac
 from src.transformers.adapters.composition import Fuse
+from src.transformers.adapters import AdapterTrainer
 #=================================================================================================
 
 dataset = load_dataset("rotten_tomatoes")
@@ -82,53 +46,53 @@ config = BigBirdConfig.from_pretrained(model_ckpt,num_labels=2,)
 # model = RobertaModelWithHeads.from_pretrained('roberta-base')
 
 # #=====================================================================================================
-# model = BigBirdModelWithHeads.from_pretrained(model_ckpt,config=config)
+model = BigBirdModelWithHeads.from_pretrained(model_ckpt,config=config)
 
-# # Add a new adapter
-# model.add_adapter("rotten_tomatoes1")
+# Add a new adapter
+model.add_adapter("rotten_tomatoes1")
 
-# # Add a matching classification head
-# model.add_classification_head(
-#     "rotten_tomatoes1",
-#     num_labels=2,
-#     id2label={ 0: "👎", 1: "👍"}
-#   )
-# # Activate the adapter
-# model.train_adapter("rotten_tomatoes1")
+# Add a matching classification head
+model.add_classification_head(
+    "rotten_tomatoes1",
+    num_labels=2,
+    id2label={ 0: "👎", 1: "👍"}
+  )
+# Activate the adapter
+model.train_adapter("rotten_tomatoes1")
 
-# print("Hey there we are ready with the model!!")
+print("Hey there we are ready with the model!!")
 
-# training_args = TrainingArguments(
-#     learning_rate=1e-4,
-#     num_train_epochs=1,
-#     per_device_train_batch_size=32,
-#     per_device_eval_batch_size=32,
-#     logging_steps=200,
-#     output_dir="./training_output",
-#     overwrite_output_dir=True,
-#     # The next line is important to ensure the dataset labels are properly passed to the model
-#     remove_unused_columns=False,
-# )
+training_args = TrainingArguments(
+    learning_rate=1e-4,
+    num_train_epochs=1,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
+    logging_steps=200,
+    output_dir="./training_output",
+    overwrite_output_dir=True,
+    # The next line is important to ensure the dataset labels are properly passed to the model
+    remove_unused_columns=False,
+)
 
 # # AdapterTrainer is throwing error : temporarily tested with Trainer
+# print("Now error will come=========================")
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=dataset["train"],
+    eval_dataset=dataset["validation"],
+)
 
-# trainer = Trainer(
-#     model=model,
-#     args=training_args,
-#     train_dataset=dataset["train"],
-#     eval_dataset=dataset["validation"],
-# )
+trainer.train()
+trainer.evaluate()
+print("Training Done !!")
 
-# trainer.train()
-# trainer.evaluate()
-# print("Training Done !!")
-
-# #Inference
-# sentence = "This is awesome!"
-# tokens = tokenizer(sentence)
-# input_ids = torch.tensor(tokenizer.convert_tokens_to_ids(tokens))
-# outputs = model(input_ids)
-# print(outputs.logits)
+#Inference
+sentence = "This is awesome!"
+tokens = tokenizer(sentence)
+input_ids = torch.tensor(tokenizer.convert_tokens_to_ids(tokens))
+outputs = model(input_ids)
+print(outputs.logits)
 
 # model.save_adapter("./adapter1", "rotten_tomatoes1")
 
@@ -187,10 +151,10 @@ config = BigBirdConfig.from_pretrained(model_ckpt,num_labels=2,)
 # print("Fusion Starting ...")
 # print("========================================================================================================")
 
-model = BigBirdModelWithHeads.from_pretrained(model_ckpt,config=config)
+# model = BigBirdModelWithHeads.from_pretrained(model_ckpt,config=config)
 
-model.load_adapter("./adapter1", source="hf", load_as="adapter1", with_head=False)
-model.load_adapter("./adapter2", source="hf", load_as="adapter2", with_head=False)
+# model.load_adapter("./adapter1", source="hf", load_as="adapter1", with_head=False)
+# model.load_adapter("./adapter2", source="hf", load_as="adapter2", with_head=False)
 
 # # Add a fusion layer for all loaded adapters
 # model.add_adapter_fusion(Fuse("adapter1", "adapter2"))
@@ -233,15 +197,15 @@ model.load_adapter("./adapter2", source="hf", load_as="adapter2", with_head=Fals
 
 
 # # Add a fusion layer for all loaded adapters
-model.load_adapter_fusion("./output_fusion")
+# model.load_adapter_fusion("./output_fusion")
 
-model.load_head("./output_fusion_head")
+# model.load_head("./output_fusion_head")
 
-model.set_active_adapters(Fuse("adapter1", "adapter2"))
+# model.set_active_adapters(Fuse("adapter1", "adapter2"))
 
-#Inference
-sentence = "This is awesome!"
-tokens = tokenizer(sentence)
-input_ids = torch.tensor(tokenizer.convert_tokens_to_ids(tokens))
-outputs = model(input_ids)
-print(outputs.logits)
+# #Inference
+# sentence = "This is awesome!"
+# tokens = tokenizer(sentence)
+# input_ids = torch.tensor(tokenizer.convert_tokens_to_ids(tokens))
+# outputs = model(input_ids)
+# print(outputs.logits)
