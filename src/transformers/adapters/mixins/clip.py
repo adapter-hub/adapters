454e7574a0747ea21e6d7abd9631c4c79699daeb
calpt <calpt@mail.de>
2023-01-19 21:19:09 +0100

    Start CLIP implementation

---
+++ src/transformers/adapters/mixins/clip.py
@@ -0,0 +1,57 @@
+from typing import Iterable, Tuple
+
+import torch.nn as nn
+
+
+from ..layer import AdapterLayer
+from ..model_mixin import (
+    EmbeddingAdaptersMixin,
+    EmbeddingAdaptersWrapperMixin,
+    InvertibleAdaptersMixin,
+    ModelAdaptersMixin,
+    ModelWithHeadsAdaptersMixin,
+)
+
+
+class CLIPEncoderLayerAdaptersMixin:
+    """Adds adapters to the CLIPEncoderLayer module of CLIP."""
+
+    def _init_adapter_modules(self):
+        self.attention_adapters = AdapterLayer("mh_adapter", self.config)
+        self.output_adapters = AdapterLayer("output_adapter", self.config)
+        self.attention_adapters._init_adapter_modules()
+        self.output_adapters._init_adapter_modules()
+
+
+class CLIPTextModelAdaptersMixin(EmbeddingAdaptersMixin, InvertibleAdaptersMixin, ModelAdaptersMixin):
+    """Adds adapters to the CLIPTextModel class."""
+
+    def iter_layers(self) -> Iterable[Tuple[int, nn.Module]]:
+        for i, layer in enumerate(self.text_model.transformer.layers):
+            yield i, layer
+
+
+class CLIPVisionModelAdaptersMixin(ModelAdaptersMixin):
+    """Adds adapters to the a CLIPVisionModel class."""
+
+    def iter_layers(self) -> Iterable[Tuple[int, nn.Module]]:
+        for i, layer in enumerate(self.vision_model.transformer.layers):
+            yield i, layer
+
+
+class CLIPModelAdaptersMixin(EmbeddingAdaptersWrapperMixin, ModelWithHeadsAdaptersMixin):
+    """Adds adapters to the CLIPModel class."""
+
+    def _init_adapter_modules(self):
+        self.invertible_adapters = self.text_model.invertible_adapters
+        self.add_invertible_adapter = self.text_model.add_invertible_adapter
+        self.get_invertible_adapter = self.text_model.get_invertible_adapter
+        self.enable_invertible_adapters = self.text_model.enable_invertible_adapters
+        self.invertible_adapters_forward = self.text_model.invertible_adapters_forward
+        super()._init_adapter_modules()
+
+    def iter_layers(self) -> Iterable[Tuple[int, nn.Module]]:
+        for i, layer in enumerate(self.text_model.transformer.layers):
+            yield i, layer
+        for i, layer in enumerate(self.vision_model.transformer.layers):
+            yield i, layer
---
+++ tests_adapters/test_clip.py
@@ -0,0 +1,129 @@
+import unittest
+
+from tests.models.clip.test_modeling_clip import *
+from transformers.testing_utils import require_torch
+
+from .methods import (
+    BottleneckAdapterTestMixin,
+    CompacterTestMixin,
+    IA3TestMixin,
+    LoRATestMixin,
+    PrefixTuningTestMixin,
+    UniPELTTestMixin,
+)
+from .test_adapter import AdapterTestBase, VisionAdapterTestBase, make_config
+from .test_adapter_backward_compability import CompabilityTestMixin
+from .test_adapter_composition import ParallelAdapterInferenceTestMixin, ParallelTrainingMixin
+from .test_adapter_fusion_common import AdapterFusionModelTestMixin
+from .test_adapter_heads import PredictionHeadModelTestMixin
+from .test_common import AdapterModelTesterMixin
+
+# TODO
+# @require_torch
+# class CLIPAdapterModelTest(CLIPModelTest):
+#     all_model_classes = (
+#         CLIPAdapterModel,
+#     )
+#     fx_compatible = False
+
+
+class CLIPVisionAdapterTestBase(VisionAdapterTestBase):
+    config_class = CLIPVisionConfig
+    config = make_config(
+        CLIPVisionConfig,
+        image_size=30,
+        hidden_size=32,
+        num_hidden_layers=4,
+        num_attention_heads=4,
+        intermediate_size=37,
+    )
+    feature_extractor_name = 'openai/clip-vit-base-patch32'
+
+
+@require_torch
+class CLIPVisionAdapterTest(
+    BottleneckAdapterTestMixin,
+    CompacterTestMixin,
+    IA3TestMixin,
+    LoRATestMixin,
+    PrefixTuningTestMixin,
+    UniPELTTestMixin,
+    AdapterFusionModelTestMixin,
+    CompabilityTestMixin,
+    PredictionHeadModelTestMixin,
+    ParallelAdapterInferenceTestMixin,
+    ParallelTrainingMixin,
+    CLIPVisionAdapterTestBase,
+    unittest.TestCase,
+):
+    pass
+
+
+class CLIPTextAdapterTestBase(AdapterTestBase):
+    config_class = CLIPTextConfig
+    config = make_config(
+        CLIPTextConfig,
+        hidden_size=32,
+        num_hidden_layers=4,
+        num_attention_heads=4,
+        intermediate_size=37,
+    )
+    tokenizer_name = 'openai/clip-vit-base-patch32'
+
+
+@require_torch
+class CLIPTextAdapterTest(
+    BottleneckAdapterTestMixin,
+    CompacterTestMixin,
+    IA3TestMixin,
+    LoRATestMixin,
+    PrefixTuningTestMixin,
+    UniPELTTestMixin,
+    AdapterFusionModelTestMixin,
+    CompabilityTestMixin,
+    PredictionHeadModelTestMixin,
+    ParallelAdapterInferenceTestMixin,
+    ParallelTrainingMixin,
+    CLIPTextAdapterTestBase,
+    unittest.TestCase,
+):
+    pass
+
+
+class CLIPAdapterTestBase(AdapterTestBase):
+    config_class = CLIPConfig
+    config = CLIPConfig.from_text_vision_configs(
+        CLIPTextConfig(
+            hidden_size=32,
+            num_hidden_layers=4,
+            num_attention_heads=4,
+            intermediate_size=37,
+        ),
+        CLIPVisionConfig(
+            image_size=30,
+            hidden_size=32,
+            num_hidden_layers=4,
+            num_attention_heads=4,
+            intermediate_size=37,
+        )
+    )
+    tokenizer_name = 'openai/clip-vit-base-patch32'
+
+
+@require_torch
+class CLIPAdapterTest(
+    BottleneckAdapterTestMixin,
+    CompacterTestMixin,
+    IA3TestMixin,
+    LoRATestMixin,
+    PrefixTuningTestMixin,
+    UniPELTTestMixin,
+    AdapterFusionModelTestMixin,
+    CompabilityTestMixin,
+    PredictionHeadModelTestMixin,
+    ParallelAdapterInferenceTestMixin,
+    ParallelTrainingMixin,
+    CLIPAdapterTestBase,
+    unittest.TestCase,
+):
+    pass
