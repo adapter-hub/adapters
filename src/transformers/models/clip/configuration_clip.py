d311b8e3c501e3a8d06713cdd7443ba17347a0e4
calpt <36051308+calpt@users.noreply.github.com>
2023-01-21 15:27:18 +0100

    Remove adapters config from sub-configs

--- src/transformers/models/clip/configuration_clip.py
+++ src/transformers/models/clip/configuration_clip.py
@@ -349,6 +349,11 @@ class CLIPConfig(PretrainedConfig):
         output["model_type"] = self.__class__.model_type
 
         self.adapters_to_dict(output)
+        # Remove adapters config from sub-configs
+        if "adapters" in output["text_config"]:
+            del output["text_config"]["adapters"]
+        if "adapters" in output["vision_config"]:
+            del output["vision_config"]["adapters"]
 
         return output
 
