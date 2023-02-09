c2a42c97fd8ba6ec7f213e98d10a1d8511ece907
calpt <36051308+calpt@users.noreply.github.com>
2023-02-09 15:53:25 +0100

    Refactor udp training script

--- src/transformers/adapters/training.py
+++ src/transformers/adapters/training.py
@@ -33,18 +33,31 @@ class AdapterArguments:
     )
 
 
-def setup_adapter_training(model, adapter_args: AdapterArguments, adapter_name: str):
+def setup_adapter_training(
+    model,
+    adapter_args: AdapterArguments,
+    adapter_name: str,
+    adapter_config_kwargs: Optional[dict] = None,
+    adapter_load_kwargs: Optional[dict] = None,
+):
     """Setup model for adapter training based on given adapter arguments.
 
     Args:
         model (_type_): The model instance to be trained.
         adapter_args (AdapterArguments): The adapter arguments used for configuration.
         adapter_name (str): The name of the adapter to be added.
+
+    Returns:
+        Tuple[str, str]: A tuple containing the names of the loaded adapters.
     """
+    if adapter_config_kwargs is None:
+        adapter_config_kwargs = {}
+    if adapter_load_kwargs is None:
+        adapter_load_kwargs = {}
     # Setup adapters
     if adapter_args.train_adapter:
         # resolve the adapter config
-        adapter_config = AdapterConfigBase.load(adapter_args.adapter_config)
+        adapter_config = AdapterConfigBase.load(adapter_args.adapter_config, **adapter_config_kwargs)
         # load a pre-trained from Hub if specified
         # note: this logic has changed in versions > 3.1.0: adapter is also loaded if it already exists
         if adapter_args.load_adapter:
@@ -52,6 +65,7 @@ def setup_adapter_training(model, adapter_args: AdapterArguments, adapter_name: 
                 adapter_args.load_adapter,
                 config=adapter_config,
                 load_as=adapter_name,
+                **adapter_load_kwargs,
             )
         # otherwise, if adapter does not exist, add it
         elif adapter_name not in model.config.adapters:
@@ -59,11 +73,12 @@ def setup_adapter_training(model, adapter_args: AdapterArguments, adapter_name: 
         # optionally load a pre-trained language adapter
         if adapter_args.load_lang_adapter:
             # resolve the language adapter config
-            lang_adapter_config = AdapterConfigBase.load(adapter_args.lang_adapter_config)
+            lang_adapter_config = AdapterConfigBase.load(adapter_args.lang_adapter_config, **adapter_config_kwargs)
             # load the language adapter from Hub
             lang_adapter_name = model.load_adapter(
                 adapter_args.load_lang_adapter,
                 config=lang_adapter_config,
+                **adapter_load_kwargs,
             )
         else:
             lang_adapter_name = None
@@ -74,8 +89,12 @@ def setup_adapter_training(model, adapter_args: AdapterArguments, adapter_name: 
             model.set_active_adapters(Stack(lang_adapter_name, adapter_name))
         else:
             model.set_active_adapters(adapter_name)
+
+        return adapter_name, lang_adapter_name
     else:
         if adapter_args.load_adapter or adapter_args.load_lang_adapter:
             raise ValueError(
                 "Adapters can only be loaded in adapters training mode.Use --train_adapter to enable adapter training"
             )
+
+        return None, None
