7fc992bd4c7d5ca14f4ed52024988a91f15f777c
Timo Imhof <timo.imhof.uni@gmail.com>
2023-10-29 09:41:23 +0100

    save changes to test shell script

--- back_comp/compare.sh
+++ back_comp/compare.sh
@@ -4,10 +4,10 @@
 SaveDir="/home/timo/workbench_l/AdapterHub/test"
 
 echo "Creating reference outputs..."
-#python create_outputs.py --path="$SaveDir"
+python create_outputs.py --path="$SaveDir"
 
 git checkout clifton/dev/x-adapters 
 ls
 
 echo "Comparing to reference outputs..."
-#python compare_outputs.py --path="$SaveDir"
+python compare_outputs.py --path="$SaveDir"
