272bda44bd6841d2105de39b8ddf8f2437bf42bb
Timo Imhof <timo.imhof.uni@gmail.com>
2023-10-29 10:35:07 +0100

    Add documentation

--- back_comp/compare.sh
+++ back_comp/compare.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-# Select a directory to save the reference outputs
-TestDir="/home/timo/workbench_l/AdapterHub/test/back_comp"
-SaveDir="/home/timo/workbench_l/AdapterHub/test"
-RepoDir="/home/timo/homeworkbench_l/AdapterHub/adapter-transformers"
+# This script performs backward compatibility tests by comparing adapter versions of different branches.
+# The goal is to check if the model output produced under the same conditions is identical between branches.
+# To do this, we need to determine a directory path to save the reference output produced by the current branch.
+# It's important that this directory is outside the adapters repository to remain accessible when switching branches.
+
+# Select a directory to save the reference outputs (must be outside the repository!)
+SaveDir="<the/directory/>"
+
+# Now, determine the branch you want to compare against.
+Branch=<branch/to/compare/against>
+
+# After setting these variables, you can execute this script from the back_comp directory using the command: `sh compare.sh`
+
 
 cd ..
-pip install -e ".[dev]"
-cd $TestDir
-pwd
+pip install -e ".[dev]"     # # Ensure that the adapters version of the current branch is installed
+cd back_comp
 
 echo "Creating reference outputs..."
 python create_outputs.py --path="$SaveDir"
+cd ..
 
-cd $RepoDir
-pwd
+ 
+git checkout $Branch        # Switch branch 
+pip install -e ".[dev]"     # Install the other adapter version
 
-git checkout clifton/dev/x-adapters 
-pip install -e ".[dev]"
-cd $TestDir
-pwd
-
+cd back_comp
 echo "Comparing to reference outputs..."
 python compare_outputs.py --path="$SaveDir"
