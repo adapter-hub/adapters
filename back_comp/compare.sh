856ba3bf88603b9b5ec4b9a8a1bc2ecc83a3e402
Timo Imhof <timo.imhof.uni@gmail.com>
2023-10-29 10:05:23 +0100

    test script

--- back_comp/compare.sh
+++ back_comp/compare.sh
@@ -3,19 +3,23 @@
 # Select a directory to save the reference outputs
 TestDir="/home/timo/workbench_l/AdapterHub/test/back_comp"
 SaveDir="/home/timo/workbench_l/AdapterHub/test"
+RepoDir="/home/timo/homeworkbench_l/AdapterHub/adapter-transformers"
 
-cd ..
-pip install -e ".[dev]"
-cd back_comp
-
-echo "Creating reference outputs..."
-#python create_outputs.py --path="$SaveDir"
-
-git checkout clifton/dev/x-adapters 
 cd ..
 pip install -e ".[dev]"
 cd $TestDir
 pwd
 
+echo "Creating reference outputs..."
+python create_outputs.py --path="$SaveDir"
+
+cd $RepoDir
+pwd
+
+git checkout clifton/dev/x-adapters 
+pip install -e ".[dev]"
+cd $TestDir
+pwd
+
 echo "Comparing to reference outputs..."
-#python compare_outputs.py --path="$SaveDir"
+python compare_outputs.py --path="$SaveDir"
