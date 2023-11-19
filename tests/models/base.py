338d53280fa0468ccdc0bc24854f7b54163527e3
Leon Engländer <leon.englaender@gmail.com>
2023-11-19 21:01:41 +0100

    fix skipping beit training_gradient_checkpointing test

--- tests/models/base.py
+++ tests/models/base.py
@@ -9,5 +9,8 @@ class AdapterModelTesterMixin:
     def check_training_gradient_checkpointing(self, gradient_checkpointing_kwargs=None):
         self.skipTest("Not applicable.")
 
+    def test_training_gradient_checkpointing(self):
+        self.skipTest("Not applicable.")
+
     def test_correct_missing_keys(self):
         self.skipTest("Not applicable.")
