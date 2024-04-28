f5e4269b24f66621c82843cac1dfbcef14a62374
Timo Imhof <timo.imhof.uni@gmail.com>
2024-04-28 18:36:14 +0200

    upload dev files for experiments

--- dev/whisper/notebook.py
+++ dev/whisper/notebook.py
@@ -1,31 +1,43 @@
-from datasets import load_dataset, DatasetDict
+# Select CUDA device index
+import os
+import sys
 
-model_name_or_path = "openai/whisper-large-v2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+model_name_or_path = "openai/whisper-small"
 language = "Marathi"
 language_abbr = "mr"
 task = "transcribe"
 dataset_name = "mozilla-foundation/common_voice_11_0"
+dataset_path = r'C:\Users\timoi\PycharmProjects\adapters\tests\fixtures\samples\common_voice_en'
+
+
+from datasets import load_dataset, DatasetDict, load_from_disk
 
 common_voice = DatasetDict()
 
-common_voice["train"] = load_dataset(dataset_name, language_abbr, split="train+validation", use_auth_token=True)
-common_voice["test"] = load_dataset(dataset_name, language_abbr, split="test", use_auth_token=True)
+#common_voice["train"] = load_dataset(dataset_name, language_abbr, split="test")
+common_voice["train"] = load_from_disk(dataset_path)
+
+print(common_voice)
 
 common_voice = common_voice.remove_columns(
     ["accent", "age", "client_id", "down_votes", "gender", "locale", "path", "segment", "up_votes"]
 )
 
-from transformers import WhisperFeatureExtractor
+
+print(common_voice)
+
+from transformers import WhisperFeatureExtractor, Seq2SeqTrainingArguments, Seq2SeqTrainer
 
 feature_extractor = WhisperFeatureExtractor.from_pretrained(model_name_or_path)
-
 from transformers import WhisperTokenizer
 
 tokenizer = WhisperTokenizer.from_pretrained(model_name_or_path, language=language, task=task)
-
 from transformers import WhisperProcessor
 
 processor = WhisperProcessor.from_pretrained(model_name_or_path, language=language, task=task)
+
+print(common_voice["train"][0])
 
 from datasets import Audio
 
@@ -44,25 +56,88 @@ def prepare_dataset(batch):
     return batch
 
 
-def convert_features(sample):
-    # split inputs and labels since they have to be of different lengths and need different padding methods
-    # first treat the audio inputs by simply returning torch tensors
-
-    input_features = [{"input_features": sample["input_features"]}]
-    conv_input_features = processor.feature_extractor.pad(input_features, return_tensors="pt")
-
-    # get the tokenized label sequences
-    label_features = [{"input_ids": sample["labels"]}]
-    # pad the labels to max length
-    conv_label_features = processor.tokenizer.pad(label_features, return_tensors="pt")
-
-    # replace padding with -100 to ignore loss correctly
-    conv_label_features = conv_label_features["input_ids"].masked_fill(conv_label_features.attention_mask.ne(1), -100)
-
-    return {"input_features": conv_input_features["input_features"], "labels": conv_label_features}
-
-
 common_voice = common_voice.map(prepare_dataset, remove_columns=common_voice.column_names["train"])
-print(common_voice)
-common_voice = common_voice.map(convert_features)
-print(common_voice)
+
+print(common_voice["train"])
+
+import torch
+
+from dataclasses import dataclass
+from typing import Any, Dict, List, Union
+
+
+@dataclass
+class DataCollatorSpeechSeq2SeqWithPadding:
+    processor: Any
+
+    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
+        # split inputs and labels since they have to be of different lengths and need different padding methods
+        # first treat the audio inputs by simply returning torch tensors
+        input_features = [{"input_features": feature["input_features"]} for feature in features]
+        batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")
+
+        # get the tokenized label sequences
+        label_features = [{"input_ids": feature["labels"]} for feature in features]
+        # pad the labels to max length
+        labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
+
+        # replace padding with -100 to ignore loss correctly
+        labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
+
+        # if bos token is appended in previous tokenization step,
+        # cut bos token here as it's append later anyways
+        if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():
+            labels = labels[:, 1:]
+
+        batch["labels"] = labels
+
+        return batch
+
+
+data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
+
+import evaluate
+
+metric = evaluate.load("wer")
+
+
+def compute_metrics(pred):
+    pred_ids = pred.predictions
+    label_ids = pred.label_ids
+
+    # replace -100 with the pad_token_id
+    label_ids[label_ids == -100] = tokenizer.pad_token_id
+
+    # we do not want to group tokens when computing the metrics
+    pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+    label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
+
+    wer = 100 * metric.compute(predictions=pred_str, references=label_str)
+
+    return {"wer": wer}
+
+
+from transformers import WhisperForConditionalGeneration
+
+model = WhisperForConditionalGeneration.from_pretrained(model_name_or_path)
+model.config.forced_decoder_ids = None
+model.config.suppress_tokens = []
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir="./examples",
+    do_train=True,
+    learning_rate=1.0,
+    max_steps=8,
+    per_device_train_batch_size=2,
+    remove_unused_columns=False,
+)
+
+trainer = Seq2SeqTrainer(
+    args=training_args,
+    model=model,
+    train_dataset=common_voice["train"],
+    data_collator=data_collator,
+    tokenizer=processor.feature_extractor,
+)
+
+trainer.train()
--- dev/whisper/train_whisper.py
+++ dev/whisper/train_whisper.py
@@ -1,15 +1,16 @@
-from transformers import AutoFeatureExtractor, AutoProcessor, AutoTokenizer, WhisperProcessor
+from typing import List, Dict, Union
+
+import torch
+from transformers import AutoFeatureExtractor, AutoProcessor, AutoTokenizer, WhisperProcessor, WhisperModel
 from datasets import load_from_disk, Audio
 
 dataset = load_from_disk("common_voice_en")
 
 feature_extractor = AutoFeatureExtractor.from_pretrained("openai/whisper-small")
 tokenizer = AutoTokenizer.from_pretrained("openai/whisper-small", language="English", task="transcribe")
-processor = WhisperProcessor.from_pretrained("openai/whisper-small", language='English', task="transcribe"
-)
+processor = WhisperProcessor.from_pretrained("openai/whisper-small", language='English', task="transcribe")
 
-# resample audio data from 48 to 16kHz
-dataset = dataset.cast_column("audio", Audio(sampling_rate=16000))
+
 
 def transform(sample):
     # Load and resample audio data from 48 kHZ to match the model's expected sampling rate
@@ -23,29 +24,94 @@ def transform(sample):
     sample["labels"] = tokenizer(sample["sentence"]).input_ids
     return sample
 
-def convert_features(sample):
+
+def prepare_dataset(batch):
+    # Load and resample audio data from 48 kHZ to match the model's expected sampling rate
+    audio = batch["audio"]
+    raw_speech = [sample["array"] for sample in audio]
+    sampling_rate = audio[0]["sampling_rate"]
+
+    # compute log-Mel input features from input audio array
+    input_features = feature_extractor(raw_speech=raw_speech, sampling_rate=sampling_rate,
+                                       return_tensors='pt').input_features
+
+    # encode target text to label ids
+    sentences = batch["sentence"]
+    print(sentences)
+    labels = tokenizer(sentences).input_ids
+
+    # return the batch
+    batch["input_features"] = input_features
+    batch["labels"] = labels
+    return batch
+
+
+def convert_features(features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
     # split inputs and labels since they have to be of different lengths and need different padding methods
     # first treat the audio inputs by simply returning torch tensors
-    new_sample = {}
-    input_features = [{"input_features": sample["input_features"]}]
-    new_sample["input_features"] = processor.feature_extractor.pad(input_features, return_tensors="pt")["input_features"]
+    batched_i_f = features["input_features"]
+    input_features = [{"input_features": feature} for feature in batched_i_f]
+    batch = processor.feature_extractor.pad(input_features, return_tensors="pt")
 
     # get the tokenized label sequences
-    label_features = [{"input_ids": sample["labels"]}]
+    labels_batched = features["labels"]
+    lables = [{"input_ids": label} for label in labels_batched]
     # pad the labels to max length
-    conv_label_features = processor.tokenizer.pad(label_features, return_tensors="pt")
+    padded_labels = processor.tokenizer.pad(lables, return_tensors="pt")
 
     # replace padding with -100 to ignore loss correctly
-    conv_label_features = conv_label_features["input_ids"].masked_fill(conv_label_features.attention_mask.ne(1), -100)
-    new_sample["input_ids"] = conv_label_features
-    return new_sample
+    labels = padded_labels["input_ids"].masked_fill(padded_labels.attention_mask.ne(1), -100)
+
+    # if bos token is appended in previous tokenization step,
+    # cut bos token here as it's append later anyways
+    if (labels[:, 0] == processor.tokenizer.bos_token_id).all().cpu().item():
+        labels = labels[:, 1:]
+
+    batch["labels"] = labels
+    batch["decoder_input_ids"] = labels
+    return batch
 
 
-dataset = dataset.map(transform, batched=False, remove_columns=dataset.column_names)
-dataset = dataset.map(convert_features, batched=False, remove_columns=["labels"])
-dataset.set_format(type='torch')
-print(dataset)
-print(dataset[0]["input_features"].shape)
+def non_batched(dataset):
+    dataset = dataset.map(transform)
+    dataset = dataset.map(convert_features, batched=True)
+    dataset.set_format(type="torch", columns=["input_features", "labels"])
+    print(dataset[0])
 
-def datacollator(sample):
-    pass
+
+def batched(dataset):
+    # Resampling audio from 48 kHZ to match the model's expected sampling rate, executed upon reading the dataset
+    dataset = dataset.cast_column("audio", Audio(sampling_rate=16000))
+
+    dataset = dataset.map(prepare_dataset, batched=True, remove_columns=dataset.column_names)
+    dataset = dataset.map(convert_features, batched=True)
+
+
+    dataset.set_format(type="torch", columns=["input_features", "labels", "decoder_input_ids"])
+    return dataset
+
+
+dataset = batched(dataset)
+print(type(dataset))
+
+def training(dataset):
+    # Training
+
+    from transformers import TrainingArguments, Trainer
+
+    training_args = TrainingArguments(
+        output_dir="./examples",
+        do_train=True,
+        learning_rate=1.0,
+        max_steps=8,
+        per_device_train_batch_size=2,
+        remove_unused_columns=False,
+    )
+
+    trainer = Trainer(
+        model=WhisperModel.from_pretrained("openai/whisper-small"),
+        args=training_args,
+        train_dataset=dataset,
+    )
+
+    trainer.train()
