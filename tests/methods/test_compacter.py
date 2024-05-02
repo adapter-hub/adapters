182f5a581c79fd352e9419722ca079bbc89a4ea7
Timo Imhof <timo.imhof.uni@gmail.com>
2024-05-02 11:31:45 +0200

    Add special check for vision models

--- tests/methods/test_compacter.py
+++ tests/methods/test_compacter.py
@@ -52,8 +52,8 @@ class CompacterTestMixin(AdapterMethodBaseTestMixin):
 
     def test_compacter_generate(self):
         if self.config_class not in ADAPTER_MODEL_MAPPING or (
-            "seq2seq_lm" not in ADAPTER_MODEL_MAPPING[self.config_class].head_types
-            and "causal_lm" not in ADAPTER_MODEL_MAPPING[self.config_class].head_types
+                "seq2seq_lm" not in ADAPTER_MODEL_MAPPING[self.config_class].head_types
+                and "causal_lm" not in ADAPTER_MODEL_MAPPING[self.config_class].head_types
         ):
             self.skipTest("No seq2seq or causal language model head")
 
@@ -69,7 +69,12 @@ class CompacterTestMixin(AdapterMethodBaseTestMixin):
         seq_output_length = 32
 
         # Finally, also check if generation works properly
-        input_ids = self.get_input_samples((1, 4), config=model1.config)["input_ids"]
+        # Check if model is a speech model, because they have a different input shape and name
+        # TODO: generalize to similar failing tests
+        if hasattr(model1.config, "num_mel_bins"):
+            input_ids = self.get_input_samples(config=model1.config)["input_features"]
+        else:
+            input_ids = self.get_input_samples((1, 4), config=model1.config)["input_ids"]
         input_ids = input_ids.to(torch_device)
         generated = model1.generate(input_ids, max_length=seq_output_length)
         self.assertLessEqual(generated.shape, (1, seq_output_length))
