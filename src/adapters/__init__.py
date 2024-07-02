ae44310fd0858fd50d275384a454f96b402d9ac3
calpt <calpt@mail.de>
2024-07-02 12:47:04 +0200

    Remove deprecated functionality

--- src/adapters/__init__.py
+++ src/adapters/__init__.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.2.2"
+__version__ = "1.0.0.dev0"
 
 from typing import TYPE_CHECKING
 
