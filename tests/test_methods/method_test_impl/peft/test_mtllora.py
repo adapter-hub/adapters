052e8a019a4c74ddae09c9e8ef37eb5615a0fd19
Francois Ledoyen <ledoyen202@C302L-G20P13.png.unicaen.fr>
2025-02-10 17:55:29 +0100

    rename MTLLoRA test method

--- tests/test_methods/method_test_impl/peft/test_mtllora.py
+++ tests/test_methods/method_test_impl/peft/test_mtllora.py
@@ -7,7 +7,7 @@ from transformers.testing_utils import require_torch
 
 @require_torch
 class MTLLoRATestMixin(AdapterMethodBaseTestMixin):
-    def test_add_mtl_lora(self):
+    def test_add_mtllora(self):
         model = self.get_model()
         config = MTLLoRAConfig()
         self.run_add_test(
@@ -18,11 +18,11 @@ class MTLLoRATestMixin(AdapterMethodBaseTestMixin):
             ],
         )
 
-    def test_leave_out_mtl_lora(self):
+    def test_leave_out_mtllora(self):
         model = self.get_model()
         self.run_leave_out_test(model, MTLLoRAConfig(), self.leave_out_layers)
 
-    def test_delete_mtl_lora(self):
+    def test_delete_mtllora(self):
         model = self.get_model()
         config = MTLLoRAConfig()
         self.run_delete_test(
@@ -33,7 +33,7 @@ class MTLLoRATestMixin(AdapterMethodBaseTestMixin):
             ],
         )
 
-    def test_get_mtl_lora(self):
+    def test_get_mtllora(self):
         model = self.get_model()
         n_layers = len(list(model.iter_layers()))
         self.run_get_test(
@@ -42,7 +42,7 @@ class MTLLoRATestMixin(AdapterMethodBaseTestMixin):
             n_layers * 3,
         )
 
-    def test_forward_mtl_lora(self):
+    def test_forward_mtllora(self):
         model = self.get_model()
         for dtype in self.dtypes_to_test:
             for n_proj in [1, 3]:
@@ -62,14 +62,14 @@ class MTLLoRATestMixin(AdapterMethodBaseTestMixin):
                         dtype=dtype,
                     )
 
-    def test_load_mtl_lora(self):
+    def test_load_mtllora(self):
         self.run_load_test(MTLLoRAConfig())
 
-    def test_load_full_model_mtl_lora(self):
+    def test_load_full_model_mtllora(self):
         self.run_full_model_load_test(MTLLoRAConfig(init_weights="bert"))
 
-    def test_train_mtl_lora(self):
+    def test_train_mtllora(self):
         self.run_train_test(MTLLoRAConfig(init_weights="bert"), ["loras.{name}."])
 
-    def test_lora_gradient_checkpointing_single_adapter(self):
+    def test_mtllora_gradient_checkpointing_single_adapter(self):
         self.run_gradient_checkpointing_single_adapter_test(MTLLoRAConfig())
