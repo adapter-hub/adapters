cff3211013e662d84474c247b62dc04dcd281531
Francois Ledoyen <ledoyen202@C302L-G20P13.png.unicaen.fr>
2025-02-12 19:18:00 +0100

    disable MTL Config Union tests for EncoderDecoder model (task_ids does not pass until decoder forward context)

--- tests/test_methods/test_on_encoder_decoder.py
+++ tests/test_methods/test_on_encoder_decoder.py
@@ -66,7 +66,15 @@ class EncoderDecoderAdapterTestBase(TextAdapterTestBase):
 
 test_methods = generate_method_tests(
     EncoderDecoderAdapterTestBase,
-    not_supported=["Heads", "ConfigUnion", "Embeddings", "Composition", "PromptTuning", "ClassConversion"],
+    not_supported=[
+        "Heads",
+        "ConfigUnion",
+        "Embeddings",
+        "Composition",
+        "PromptTuning",
+        "ClassConversion",
+        "MultiTaskConfigUnion",
+    ],
 )
 
 for test_class_name, test_class in test_methods.items():
