50dcca37f575f6d9c64171407098020f87cf2d77
Francois Ledoyen <ledoyen202@C302L-G20P13.png.unicaen.fr>
2025-02-13 01:27:07 +0100

    add load test for MTL conf union

--- tests/test_methods/method_test_impl/peft/test_config_mtl_union.py
+++ tests/test_methods/method_test_impl/peft/test_config_mtl_union.py
@@ -1,7 +1,12 @@
+from accelerate.state import torch
+from accelerate.utils.modeling import tempfile
 from adapters.composition import MultiTask
 from adapters.configuration.adapter_config import MTLLoRAConfig, MultiTaskConfigUnion
 from adapters.context import ForwardContext
+from adapters.utils import WEIGHTS_NAME
+from huggingface_hub import os
 from tests.test_methods.method_test_impl.base import AdapterMethodBaseTestMixin
+from tests.test_methods.method_test_impl.utils import create_twin_models
 from transformers.testing_utils import require_torch, torch_device
 
 
@@ -104,3 +109,43 @@ class MultiTaskConfigUnionAdapterTest(AdapterMethodBaseTestMixin):
                     n_tasks=len(adapter_config.task_names),
                     adapter_setup=MultiTask(*adapter_config.task_names),
                 )
+
+    def run_load_test(self, adapter_config, **kwargs):
+        model1, model2 = create_twin_models(self.model_class, self.config)
+
+        name = "dummy_adapter"
+        model1.add_adapter(name, config=adapter_config)
+        model1.set_active_adapters(name)
+        with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as temp_dir:
+            model1.save_adapter(temp_dir, name)
+
+            # Check that there are actually weights saved
+            # empty string is for union shared shared params.
+            for adapter_name in ["", *adapter_config.task_names]:
+                weights = torch.load(
+                    os.path.join(temp_dir, adapter_name, WEIGHTS_NAME),
+                    map_location="cpu",
+                    weights_only=True,
+                )
+                self.assertTrue(len(weights) > 0)
+
+            # also tests that set_active works
+            loading_info = {}
+            model2.load_adapter(temp_dir, set_active=True, loading_info=loading_info)
+
+        # check if all weights were loaded
+        self.assertEqual(0, len(loading_info["missing_keys"]))
+        self.assertEqual(0, len(loading_info["unexpected_keys"]))
+
+        # check if adapter was correctly loaded
+        for adapter_name in [name, *adapter_config.task_names]:
+            self.assertTrue(adapter_name in model2.adapters_config)
+
+        # check equal output
+        input_data = self.get_input_samples(config=model1.config, **kwargs)
+        model1.to(torch_device)
+        model2.to(torch_device)
+        output1 = model1(**input_data)
+        output2 = model2(**input_data)
+        self.assertEqual(len(output1), len(output2))
+        self.assertTrue(torch.allclose(output1[0], output2[0], atol=1e-4))
