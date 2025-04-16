b633433d1c6fe97f1e6c5de39b3e0840d4e9335a
Timo Imhof <timo.imhof.uni@gmail.com>
2025-04-16 21:11:35 +0200

    Fix invertible adapter forward pass: - Add post_embedding_forward hook logic and hook registration to the text model - Without it invertible adapters are not invoked although added to the model

--- src/adapters/models/mllama/mixin_mllama.py
+++ src/adapters/models/mllama/mixin_mllama.py
@@ -86,6 +86,17 @@ class MllamaTextModelAdaptersMixin(EmbeddingAdaptersMixin, InvertibleAdaptersMix
         for i, layer in enumerate(self.layers):
             yield i, layer
 
+    def init_adapters(self, model_config, adapters_config):
+        super().init_adapters(model_config, adapters_config)
+
+        # Register hook for post embedding forward
+        self.embed_tokens.register_forward_hook(self.post_embedding_forward)
+
+    def post_embedding_forward(self, module, args, embedding_output):
+        embedding_output = self.invertible_adapters_forward(embedding_output)
+        # Prompt tuning not yet supported
+        return embedding_output
+
 
 class MllamaAdaptersMixin(EmbeddingAdaptersWrapperMixin, InvertibleAdaptersWrapperMixin, ModelBaseAdaptersMixin):
     """
