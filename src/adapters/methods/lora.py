cca4c54faa4611be0e2244894c04d1babb4428ab
julian fong <cyfongj@gmail.com>
2025-04-20 20:56:25 -0400

    fixed bug

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -496,38 +496,43 @@ class DoRA(nn.Module):
         weights: torch.Tensor,
         added: torch.Tensor,
         input_states: torch.Tensor = None,
+        frozen_pretrained_weights: torch.Tensor = None,
         scaling=None,
     ) -> torch.Tensor:
         """Performs the composition operation between existing and injected weights.
 
-        During training, the compose method requires the frozen pretrained weights (W_o)
-        of the layer in order to calculate the norm, as well as the input_states `x`
-        to calculate the updated hidden states.
+        This compose method requires the frozen pretrained weights (W_o) of the layer
+        in order to calculate the norm, as well as the input_states `x` to calculate the
+        updated hidden states.
 
-        In the case where `input_states` are not passed, we will return the
-        magnitude component multiplied with the direction component (used for merging).
+        In the case where `input_states` and `frozen_pretrained_weights` are not passed,
+        we will return the magnitude component multiplied with the direction component
+        (used for merging).
+
         """
         if scaling is None:
             scaling = self.scaling
 
         # if input_states `x` and the frozen_pretrained_weights is passed, we are training
-        if input_states is not None:
+        if input_states is not None and frozen_pretrained_weights is not None:
+            # v = W_o + BA used to calculate norm_scale
             # we save the current state v in case we need v to calculate the com_inv
-            v = self.w_o + (self.lora_B @ self.lora_A) * self.scaling
+            v = frozen_pretrained_weights + (self.lora_B @ self.lora_A) * self.scaling
+            self.v = v
             # norm_scale = m / ||W_o + BA||c
             norm_scale = self.m.weight.view(-1) / torch.linalg.norm(v, dim=1)
-            
+
             input_states_with_dropout = self.lora_dropout(input_states)
 
-            scaled_weights = (norm_scale - 1) * F.linear(input_states_with_dropout, self.w_o)
+            scaled_weights = norm_scale * F.linear(input_states_with_dropout, frozen_pretrained_weights)
             scaled_lora = norm_scale * added
-            # result = W_ox + norm_scale * W_ox + norm_scale * BAx
+            # result = W_ox + norm_scale * W_ox + norm_scale * BA
             result = weights + scaled_weights + scaled_lora * self.scaling
         else:
             # we are merging
             v = weights + added * self.scaling
-            norm_scale = self.m.weight / torch.linalg.norm(v, dim=1).unsqueeze(1)
-            result = norm_scale * v
+            norm_scale = self.m.weight.view(-1) / torch.linalg.norm(v, dim=1)
+            result = v * norm_scale
         return result
 
     def com_inv(self, weights: torch.Tensor, added: torch.Tensor) -> torch.Tensor:
@@ -995,9 +1000,10 @@ class LoRALinear(LoRALayer, ComposableAdapterLayerBase):
                 _, hidden_states, layer_output, last = state
 
                 last_lora = self.loras[last]
-                # if we're using dora, and input states `x`` as well
+                # if we're using dora, we need original pretrained weights and input states `x`` as well
                 if last_lora.composition_mode == "dora":
-                    layer_output = last_lora.com(layer_output, hidden_states, input_states, scaling=1.0)
+                    weight = torch.transpose(self.weight, -2, -1) if self.fan_in_fan_out else self.weight
+                    layer_output = last_lora.com(layer_output, hidden_states, input_states, weight, scaling=1.0)
                 else:
                     layer_output = last_lora.com(
                         layer_output, hidden_states, scaling=1.0
